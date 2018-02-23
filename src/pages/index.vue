e5b32ecf1d622d1f1eadefd8b6cd75e0b07591c4
lyb12358 <69788721@qq.com>
2018-02-23 17:26:23 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img src="~assets/personal_logo.jpg">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
