77a7e50d4ef8034b2f9bba5c60da352c139cf5ed
lyb12358 <69788721@qq.com>
2018-06-03 19:12:22 +0800

    update

--- src/pages/changeLog.vue
+++ src/pages/changeLog.vue
@@ -1,7 +1,8 @@
 <template>
   <q-page no-padding>
     <div class="row justify-center">
-    <vue-simple-markdown :source="changeLog"></vue-simple-markdown>
+      <vue-simple-markdown :source="changeLog"
+                           style="min-width:600px"></vue-simple-markdown>
     </div>
   </q-page>
 </template>
@@ -19,6 +20,6 @@ export default {
 </script>
 
 <style lang="stylus">
-  .markdown-body
-    font-family 'Roboto', 'Microsoft YaHei', '-apple-system', 'Helvetica Neue', Helvetica, Arial, sans-serif
+.markdown-body
+  font-family 'Roboto', 'Microsoft YaHei', '-apple-system', 'Helvetica Neue', Helvetica, Arial, sans-serif
 </style>
